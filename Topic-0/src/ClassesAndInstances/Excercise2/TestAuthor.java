f670525c5ae651326b5d8f93602acc325742a37a
aepedraza <aepedraza3117@gmail.com>
2015-03-19 13:07:35 -0300

    Better files organization

---
+++ Topic-0/src/ClassesAndInstances/Excercise2/TestAuthor.java
@@ -0,0 +1,19 @@
+package Excercise2;
+
+/**
+ *
+ * @author apedraza
+ */
+public class TestAuthor {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Author anAuthor = new Author("Tan Ah Teck", "ahteck@somewhere.com", 'm');
+    System.out.println(anAuthor);   // call toString()
+    anAuthor.setEmail("paul@nowhere.com");
+    System.out.println(anAuthor);
+  }
+
+}
---
+++ Topic-0/src/ClassesAndInstances/Excercise3/TestPoints.java
@@ -0,0 +1,31 @@
+package Excercise3;
+
+/**
+ *
+ * @author apedraza
+ */
+public class TestPoints {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    MyPoint p1 = new MyPoint();
+    MyPoint p2 = new MyPoint(3, 4);
+    MyPoint p3 = new MyPoint();
+    
+    p1.setX(0);
+    p1.setY(3);
+    p3.setXY(5, 5);
+    
+    System.out.println("p1: " + p1);
+    System.out.println("p2: " + p2.toString());
+    System.out.println("distance p1-p2: " + p1.distance(p2));
+    System.out.println("distance p1-(0, 0): " + p3.distance(0, 0));
+    
+    MyPoint[] points = new MyPoint[10];
+    for (int i = 0; i < points.length; i++) points[i] = new MyPoint(i + 1, i + 1);
+    for (MyPoint p : points) System.out.print(p);
+  }
+  
+}
