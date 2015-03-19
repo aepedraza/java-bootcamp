138f25cf3bfe2b9e816a8e063991e00d65e94063
aepedraza <aepedraza3117@gmail.com>
2015-03-19 12:16:44 -0300

    Topic 0: Excercise 2

---
+++ Topic-0/src/Excercise2/Author.java
@@ -0,0 +1,39 @@
+package Excercise2;
+
+/**
+ *
+ * @author apedraza
+ */
+public class Author {
+  private String name;
+  private String email;
+  private char gender;
+
+  public Author(String name, String email, char gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public char getGender() {
+    return gender;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  
+  @Override
+  public String toString() {
+    // returns "author-name (gender) at email", e.g., "Tan Ah Teck (m) at ahTeck@somewhere.com".
+    return this.name + " (" + this.gender + ") at " + this.email;
+  }
+}
