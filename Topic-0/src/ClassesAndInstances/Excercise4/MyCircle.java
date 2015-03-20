6fb2808ee59f74e71bce4454cd2caa3973ac2acf
aepedraza <aepedraza3117@gmail.com>
2015-03-20 12:30:38 -0300

    Topic 0: Excercise 1.4

---
+++ Topic-0/src/ClassesAndInstances/Excercise4/MyCircle.java
@@ -0,0 +1,60 @@
+package ClassesAndInstances.Excercise4;
+
+import Excercise3.MyPoint;
+
+/**
+ *
+ * @author apedraza
+ */
+public class MyCircle {
+  private MyPoint center;
+  private int radius;
+
+  public MyCircle(MyPoint center, int radius) {
+    this.center = center;
+    this.radius = radius;
+  }
+
+  public MyCircle(int x, int y, int radius) {
+    this.center = new MyPoint(x, y);
+    this.radius = radius;
+  }
+
+  public MyPoint getCenter() {
+    return center;
+  }
+
+  public void setCenter(MyPoint center) {
+    this.center = center;
+  }
+
+  public int getRadius() {
+    return radius;
+  }
+
+  public void setRadius(int radius) {
+    this.radius = radius;
+  }
+  
+  public int getCenterX() {
+    return this.center.getX();
+  }
+  
+  public int getCenterY() {
+    return this.center.getY();
+  }
+  
+  public void setCenterXY(int x, int y) {
+    this.center.setXY(x, y);
+  }
+
+  @Override
+  public String toString() {
+    return "Circle @ (" + this.getCenterX() + ", " + this.getCenterY()
+            + ") radius = " + this.getRadius();
+  }
+  
+  public double getArea() {
+    return Math.PI + this.getRadius() * this.getRadius();
+  }
+}
---
+++ Topic-0/src/ClassesAndInstances/Excercise4/TestMyCircle.java
@@ -0,0 +1,33 @@
+package ClassesAndInstances.Excercise4;
+
+import Excercise3.MyPoint;
+
+/**
+ *
+ * @author apedraza
+ */
+public class TestMyCircle {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    MyCircle c1 = new MyCircle(0, 0, 3);
+    MyCircle c2 = new MyCircle(new MyPoint(2, 4), 5);
+    
+    System.out.println("Testing methods toString() & getters:");
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println("Center of c1: " + c1.getCenter());
+    System.out.println("Area of c2: " + c1.getArea());
+    
+    c1.setCenter(new MyPoint(-2, -3));
+    c2.setCenterXY(6, 5);
+    c1.setRadius(4);
+    
+    System.out.println("Result of setters:");
+    System.out.println(c1.toString());
+    System.out.println(c2.toString());
+  }
+  
+}
