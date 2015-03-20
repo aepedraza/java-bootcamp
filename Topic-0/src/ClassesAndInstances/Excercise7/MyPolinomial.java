ba427b3743b65dfce3058d5a3bcbcc18bf13542f
aepedraza <aepedraza3117@gmail.com>
2015-03-20 18:12:53 -0300

    Topic 0: Excercise 1.7 (incomplete)

---
+++ Topic-0/src/ClassesAndInstances/Excercise7/MyPolinomial.java
@@ -0,0 +1,61 @@
+package ClassesAndInstances.Excercise7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author apedraza
+ */
+public class MyPolinomial {
+  private double[] coeffs;
+
+  public MyPolinomial(double... coeffs) {
+    this.coeffs = coeffs;
+  }
+
+  public MyPolinomial(String fileName) {
+    Scanner in = null;
+    
+    try {
+      in = new Scanner(new File(fileName));
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    int degree = in.nextInt();
+    this.coeffs = new double[degree + 1];
+    for (int i = 0; i < this.coeffs.length; i++) this.coeffs[i] = in.nextDouble();
+  }
+  
+  public int getDegree() {
+    //for degree = n we have n+1 coeffs (cn, cn-1,..., c0) 
+    return this.coeffs.length - 1;
+  }
+
+  @Override
+  public String toString() {
+    String result = "";
+    for (int i = this.coeffs.length - 1; i > 0; i--) {
+      if (this.coeffs[i] == 0) continue;
+      result += this.coeffs[i] == 1.0 ? "x^" + i + " + " : this.coeffs[i] + "x^" + i + " + ";
+    }
+    result += this.coeffs[0];
+    return result;
+  }
+  
+  public double evaluate(double x) {
+    double result = 0.0;
+    for(int i = 0; i < this.coeffs.length; i++) result += this.coeffs[i] * Math.pow(x, i);
+    return result;
+  }
+  
+  public MyPolinomial add(MyPolinomial another) {
+    double[] newCoeffs = new double[this.coeffs.length >= another.coeffs.length ?
+            this.coeffs.length : another.coeffs.length];
+    
+    for(int i = 0; i < this.coeffs.length; i++) newCoeffs[i] = this.coeffs[i];
+    for(int i = 0; i < another.coeffs.length; i++) newCoeffs[i] += another.coeffs[i];
+    return new MyPolinomial(newCoeffs);
+  }
+}
