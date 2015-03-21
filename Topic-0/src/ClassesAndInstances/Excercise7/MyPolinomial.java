6decdb14189bced38a299f20c04c2ae94c7e43c9
aepedraza <aepedraza3117@gmail.com>
2015-03-20 22:07:09 -0300

    Topic 0: Excercise 1.7 (complete)

--- Topic-0/src/ClassesAndInstances/Excercise7/MyPolinomial.java
+++ Topic-0/src/ClassesAndInstances/Excercise7/MyPolinomial.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
  * @author apedraza
  */
 public class MyPolinomial {
+
   private double[] coeffs;
 
   public MyPolinomial(double... coeffs) {
@@ -17,17 +18,19 @@ public class MyPolinomial {
 
   public MyPolinomial(String fileName) {
     Scanner in = null;
-    
+
     try {
       in = new Scanner(new File(fileName));
-    } catch(FileNotFoundException e) {
+    } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
-    int degree = in.nextInt();
+    int degree = Integer.parseInt(in.next());
     this.coeffs = new double[degree + 1];
-    for (int i = 0; i < this.coeffs.length; i++) this.coeffs[i] = in.nextDouble();
+    for (int i = 0; i < this.coeffs.length; i++) {
+      this.coeffs[i] = Double.parseDouble(in.next());
+    }
   }
-  
+
   public int getDegree() {
     //for degree = n we have n+1 coeffs (cn, cn-1,..., c0) 
     return this.coeffs.length - 1;
@@ -36,26 +39,47 @@ public class MyPolinomial {
   @Override
   public String toString() {
     String result = "";
-    for (int i = this.coeffs.length - 1; i > 0; i--) {
-      if (this.coeffs[i] == 0) continue;
+    for (int i = this.coeffs.length - 1; i > 1; i--) {
+      if (this.coeffs[i] == 0) {
+        continue;
+      }
       result += this.coeffs[i] == 1.0 ? "x^" + i + " + " : this.coeffs[i] + "x^" + i + " + ";
     }
+    result += this.coeffs[1] + "x + ";
     result += this.coeffs[0];
     return result;
   }
-  
+
   public double evaluate(double x) {
     double result = 0.0;
-    for(int i = 0; i < this.coeffs.length; i++) result += this.coeffs[i] * Math.pow(x, i);
+    for (int i = 0; i < this.coeffs.length; i++) {
+      result += this.coeffs[i] * Math.pow(x, i);
+    }
     return result;
   }
-  
+
   public MyPolinomial add(MyPolinomial another) {
-    double[] newCoeffs = new double[this.coeffs.length >= another.coeffs.length ?
-            this.coeffs.length : another.coeffs.length];
-    
-    for(int i = 0; i < this.coeffs.length; i++) newCoeffs[i] = this.coeffs[i];
-    for(int i = 0; i < another.coeffs.length; i++) newCoeffs[i] += another.coeffs[i];
+    double[] newCoeffs = new double[this.coeffs.length >= another.coeffs.length
+            ? this.coeffs.length : another.coeffs.length];
+
+    for (int i = 0; i < this.coeffs.length; i++) {
+      newCoeffs[i] = this.coeffs[i];
+    }
+    for (int i = 0; i < another.coeffs.length; i++) {
+      newCoeffs[i] += another.coeffs[i];
+    }
+    return new MyPolinomial(newCoeffs);
+  }
+
+  public MyPolinomial multiply(MyPolinomial another) {
+    double[] newCoeffs = new double[this.coeffs.length + another.coeffs.length];
+
+    for (int i = 0; i < this.coeffs.length; i++) {
+      for (int j = 0; j < another.coeffs.length; j++) {
+        newCoeffs[i + j] += this.coeffs[i] + another.coeffs[j];
+      }
+    }
+
     return new MyPolinomial(newCoeffs);
   }
 }
---
+++ Topic-0/src/ClassesAndInstances/Excercise7/TestMyPolinomial.java
@@ -0,0 +1,36 @@
+package ClassesAndInstances.Excercise7;
+
+/**
+ *
+ * @author Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
+ */
+public class TestMyPolinomial {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    MyPolinomial a = new MyPolinomial(2.0, 1.5, 3.0, 0.0, 8.0);
+    MyPolinomial b = new MyPolinomial("/media/DATOS/CODIGO/java-bootcamp/Topic-0/src/ClassesAndInstances/Excercise7/polinomialConstructor");
+//    MyPolinomial b = new MyPolinomial(4, 0, 2, 3.3);
+    
+    System.out.println("degree(a) = " + a.getDegree());
+    System.out.println("degree(b) = " + b.getDegree());
+    System.out.println();
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println();
+    
+    System.out.println("a(3.5) = " + a.evaluate(3.5));
+    System.out.println("b(4.0) = " + b.evaluate(4.0));
+    System.out.println();
+    
+    System.out.println("a(x) + b(x) = " + a.add(b));
+    System.out.println("b(x) + a(x) = " + b.add(a));
+    System.out.println();
+    
+    System.out.println("a(x) * b(x)" + a.multiply(b));
+  }
+  
+}
